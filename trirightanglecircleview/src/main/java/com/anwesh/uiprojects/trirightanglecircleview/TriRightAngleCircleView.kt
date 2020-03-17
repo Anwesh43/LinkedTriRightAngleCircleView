dcbb456531407d1cc4806ab00864b617b56c632d
Anwesh43 <anweshthecool0@gmail.com>
2020-03-17 12:03:57 +0530

    adding constants and libraries

--- trirightanglecircleview/src/main/java/com/anwesh/uiprojects/trirightanglecircleview/TriRightAngleCircleView.kt
+++ trirightanglecircleview/src/main/java/com/anwesh/uiprojects/trirightanglecircleview/TriRightAngleCircleView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.trirightanglecircleview
  * Created by anweshmishra on 17/03/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 3
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 25
+
