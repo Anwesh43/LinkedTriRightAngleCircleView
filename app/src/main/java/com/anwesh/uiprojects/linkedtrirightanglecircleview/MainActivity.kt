729e9a63bd36794e19339ce2b64caa9468e1ff8a
Anwesh43 <anweshthecool0@gmail.com>
2020-03-17 13:43:04 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtrirightanglecircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrirightanglecircleview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedtrirightanglecircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.trirightanglecircleview.TriRightAngleCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriRightAngleCircleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
