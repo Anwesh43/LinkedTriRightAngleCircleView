99a6055a6666cb4ca3fcdc91f8ed060218870306
Anwesh43 <anweshthecool0@gmail.com>
2020-03-17 12:33:56 +0530

    adding State class

--- trirightanglecircleview/src/main/java/com/anwesh/uiprojects/trirightanglecircleview/TriRightAngleCircleView.kt
+++ trirightanglecircleview/src/main/java/com/anwesh/uiprojects/trirightanglecircleview/TriRightAngleCircleView.kt
@@ -20,6 +20,7 @@ val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 25
 val rFactor : Float = 6f
+val scGap : Float = 0.02f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -71,5 +72,25 @@ class TriRightAngleCircleView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
